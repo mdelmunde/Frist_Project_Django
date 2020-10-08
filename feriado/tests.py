b06f72873ea95a791e50425959f2f64b39137a39
Matheus Delmunde <matheusdelmunde@hotmail.com>
2020-10-08 16:29:17 -0300

    feriado v.0.3

--- feriado/tests.py
+++ feriado/tests.py
@@ -2,10 +2,37 @@ from django.test import TestCase
 
 class NatalTest(TestCase):
     def setUp(self):
-        self.resp = self.client.get('/')
+        self.resp = self.client.get('/natal')
 
     def test_200_response(self):
         self.assertEqual(200, self.resp.status_code)
     
     def test_texto(self):
         self.assertContains(self.resp, 'natal')
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'natal.html')    
+
+    def setUp(self):
+        self.resp = self.client.get('/carnaval')
+
+    def test_200_response(self):
+        self.assertEqual(200, self.resp.status_code)
+    
+    def test_texto(self):
+        self.assertContains(self.resp, 'carnaval')
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'carnaval.html')  
+
+    def setUp(self):
+        self.resp = self.client.get('/tiradentes')
+
+    def test_200_response(self):
+        self.assertEqual(200, self.resp.status_code)
+    
+    def test_texto(self):
+        self.assertContains(self.resp, 'tiradentes')
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'tiradentes.html')  
--- feriado/urls.py
+++ feriado/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [    
-    path('', views.natal),
+    path('natal', views.natal),
+    path('carnaval', views.carnaval),
+    path('tiradentes', views.tiradentes),
 ]
